b305038938a8dc70fd0ec2a2f6bf124885fc0b5d
K-JC <kellytolfts93@hotmail.com>
2023-08-22 14:21:22 +0000

    add comments app, create comments model and add serilizer.py file, begin imports and class creation

---
+++ comments/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Comment
+
+
+class CommentSerializer(models.Model):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
