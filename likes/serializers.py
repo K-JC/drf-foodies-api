8d668163b5c4e8f388ab23442da2b02693f47fba
K-JC <kellytolfts93@hotmail.com>
2023-08-22 15:38:08 +0000

    add function that handles errors, possible duplicate

--- likes/serializers.py
+++ likes/serializers.py
@@ -11,3 +11,12 @@ class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ['id', 'created_at', 'owner', 'post']
+
+    def create(self, validated_data):
+        """
+        Handles duplicated likes by the same user
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({'detail': 'possible duplicate'})
