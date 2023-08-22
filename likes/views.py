b1bae24cebd1a9a5080cacddf632101d21a4c288
K-JC <kellytolfts93@hotmail.com>
2023-08-22 16:08:57 +0000

    add follower class to followers model, add followers to installed apps and make migrations

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_foodies_api.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    Will list all likes and create a like if user is authenticated
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializers):
+        serializers.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like or delete a like
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
