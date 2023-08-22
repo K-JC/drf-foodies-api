280138a3ae669db23b535f9884e48d390c1b21f3
K-JC <kellytolfts93@hotmail.com>
2023-08-22 14:28:20 +0000

    add meta class and define get_is_owner to serializer

--- comments/models.py
+++ comments/models.py
@@ -5,7 +5,7 @@ from posts.models import Post
 
 class Comment(models.Model):
     """
-    Comment model which is related to User/Post 
+    Comment model which is related to User/Post
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -18,4 +18,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
-
