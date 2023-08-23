52fcb6dfd7018586ea81b05ea9b8605961829cf9
K-JC <kellytolfts93@hotmail.com>
2023-08-23 13:36:02 +0000

    finish adding json web token, add serializer to drffoodiesapi for authentication, migrate changes, update the requirements file

--- drf_foodies_api/settings.py
+++ drf_foodies_api/settings.py
@@ -36,6 +36,7 @@ REST_USE_JWT = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_SECURE = True
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'drf_foodies_api.serializers.CurrentUserSerializer'}
 
 
 # Quick-start development settings - unsuitable for production
