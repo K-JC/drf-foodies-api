da04972a73a3ce563cfab7a798c476499ec60000
K-JC <kellytolfts93@hotmail.com>
2023-08-23 15:18:15 +0000

    correct spelling error in settings.py

--- drf_foodies_api/settings.py
+++ drf_foodies_api/settings.py
@@ -39,7 +39,7 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d %b %Y'
     }
 if 'DEV' not in os.environ:
-    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = ['restframework.renderers.JSONRenderer']
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = ['rest_framework.renderers.JSONRenderer']
 
 REST_USE_JWT = True
 JWT_AUTH_COOKIE = 'my-app-auth'
