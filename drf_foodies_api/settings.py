538a695b297d72bc0b6e5b388f38819197cc1799
K-JC <kellytolfts93@hotmail.com>
2023-08-23 15:00:11 +0000

    fix log out bug

--- drf_foodies_api/settings.py
+++ drf_foodies_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import re
 import dj_database_url
 
 if os.path.exists('env.py'):
@@ -38,14 +39,14 @@ REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d %b %Y'
     }
 if 'DEV' not in os.environ:
-    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        'restframework.renderers.JSONRenderer'
-    ]
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = ['restframework.renderers.JSONRenderer']
 
 REST_USE_JWT = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_SECURE = True
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
+
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER':
     'drf_foodies_api.serializers.CurrentUserSerializer'
@@ -62,12 +63,20 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    'drf-foodies-api.herokuapp.com',
-    '8000-kjc-drffoodiesapi-nl39pdpd3bw.ws-eu104.gitpod.io',
-    'localhost'
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+    '8000-kjc-drffoodiesapi-nl39pdpd3bw.ws-eu104.gitpod.io'
     ]
 
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
 
+CORS_ALLOW_CREDENTIALS = True
 # Application definition
 
 INSTALLED_APPS = [
@@ -90,7 +99,6 @@ INSTALLED_APPS = [
     'dj_rest_auth.registration',
     'corsheaders',
 
-
     'profiles',
     'posts',
     'comments',
@@ -111,18 +119,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
-    ]
-CORS_ALLOW_CREDENTIALS = True
-JWT_AUTH_SAMESITE = 'None'
-
 
 ROOT_URLCONF = 'drf_foodies_api.urls'
 
